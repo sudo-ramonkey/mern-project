4d3edaf3d94967dd8d49d4af463771a5dea6b46f
sudo-ramonkey <j.ramonroz@gmail.com>
2025-03-27 15:00:37 -0600

    feat: Release the compiled image to github

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {errorHandler} = require('./middleware/errorMiddleware');
 const DotEnv = require('dotenv').config();
-const puerto = process.env.PUERTO || 5000;
+const puerto = process.env.PUERTO || 8000;
 const DBConexion = require('./config/database');
 const app = express();
 
